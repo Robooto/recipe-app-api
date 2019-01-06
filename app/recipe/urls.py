a50ffc6aacdfdec474d723ca1ba5659206ff408b
Sam Russell <samrussell63@gmail.com>
2019-01-05 16:48:50 -0800

    added tag list endpoint

---
+++ app/recipe/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+# router will generate urls from a viewset
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+# for the reverse function to find the url
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
